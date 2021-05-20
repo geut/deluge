3fb1807f4f37e05ef8258c8bd4752826e261a7a2
Martin Acosta <tinchoz49@gmail.com>
2021-05-20 09:50:16 -0300

    export TimestampSeq

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const Deluge = require('./deluge')
 const Packet = require('./packet')
 const Peer = require('./peer')
+const { TimestampSeq } = require('./timestamp-seq')
 const delugeNetworkSetup = require('./network-setup')
 
-module.exports = { Deluge, Packet, Peer, delugeNetworkSetup }
+module.exports = { Deluge, Packet, Peer, TimestampSeq, delugeNetworkSetup }
